e50e1b0c3addcba9159589dbed972372d8933f5c
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-05-03 12:25:58 +0200

    test

--- grade.sh
+++ grade.sh
@@ -1,23 +1,20 @@
-echo "$API_URL" "$REPO_URL" "$REPO_URL" "$REPO_URL"
-
-curl -X GET $API_URL
+echo "$API_URL" "$REPO_URL" "$REPO" "$ACTOR"
 
 sudo apt-get update
 apt-get install -y python3-pip
 
 # Install packages with pip
 pip3 install pytest pytest-cov pytest-json-report requests
+
+# Generate JSON report
 python -m pytest --json-report -v --tb=line --json-report-indent=2
 
-pip3 install pytest pytest-cov pytest-json-report
+# Remove non-printable characters from report
+cat .report.json | tr -d '\r\n' | sed 's/[^[:print:]]//g' > .report_clean.json
 
-cat .report.json | tr -d '\r\n' | sed 's/[^[:print:]]//g' > .report.json
+CONTENTS=$(cat .report_clean.json)
 
-CONTENTS=$(cat .report.json)
-
-#python python_parse.py API_URL REPO_URL REPO ACTOR CONTENTS
-echo "$CONTENTS"
-
+# Call API to submit report
 curl -X POST \
   -H "Content-Type: application/json" \
   -d '{
@@ -27,4 +24,3 @@ curl -X POST \
     "submission": "'"$CONTENTS"'"
   }' \
   $API_URL
-
