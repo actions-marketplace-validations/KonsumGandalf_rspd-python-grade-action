84b501d46b673f58cf9418b59af9fc0551f189fa
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-05-03 14:31:04 +0200

    iugabhoihbiubbhujiubiujh

--- grade.sh
+++ grade.sh
@@ -18,23 +18,27 @@ python -m pytest --json-report -v --json-report-indent=2
 # cat .report.json | tr -d '\r\n' | sed 's/[^[:print:]]//g; s|[\\/]||g' > .report_clean.json
 
 CONTENTS=$(cat .report.json | jq .)
-echo "$CONTENTS"
 
-echo '{
-    "repositoryUrl": "'"$REPO_URL"'",
-    "actor": "'"$ACTOR"'",
-    "repository": "'"$REPO"'",
-    "submission": "'"$CONTENTS"'"
-}' > .submission.json
+#echo '{
+#    "repositoryUrl": "'"$REPO_URL"'",
+#    "actor": "'"$ACTOR"'",
+#    "repository": "'"$REPO"'",
+#    "submission": "'"$CONTENTS"'"
+#}' > .submission.json
 #
 #TEST=$(cat .submission.json)
 #echo "$TEST"
 #
 #
 #curl -v -X POST "Content-Type: application/json" -d @.submission.json $API_URL
-cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}'
+#cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}'
+#
+#cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}' | curl -v -X POST "Content-Type: application/json" -d @- $API_URL
 
-cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}' | curl -v -X POST "Content-Type: application/json" -d @- $API_URL
+PAYLOAD=$(jq -n --arg repoUrl "$REPO_URL" --arg actor "$ACTOR" --arg repo "$REPO" --arg contents "$CONTENTS" '{repositoryUrl: $repoUrl, actor: $actor, repository: $repo, submission: $contents}')
+
+# Send POST request with payload
+echo "$PAYLOAD" | curl -v -X POST -H "Content-Type: application/json" -d @- "$API_URL"
 
 # Call API to submit report
 
