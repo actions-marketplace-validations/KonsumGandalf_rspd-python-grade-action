05d11dce35906ea6c3cf29c65060a0fae4cb4650
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-05-03 15:31:27 +0200

    test

--- api-request.py
+++ api-request.py
@@ -1,33 +1,33 @@
 import json
 import requests
+import os
 
 # Read report data from .report.json file
+
+
+
+
+api_url = os.environ.get("API_URL")
+repo_url = os.environ.get("REPO_URL")
+repo = os.environ.get("REPO")
+actor = os.environ.get("ACTOR")
+
 with open('.report.json', 'r') as f:
     report_data = json.load(f)
 
-# Read submission data from .submission.json file
-with open('.submission.json', 'r') as f:
-    submission_data = json.load(f)
-
-# Extract required data from submission_data
-repositoryUrl = submission_data[0]['repositoryUrl']
-actor = submission_data[0]['actor']
-repository = submission_data[0]['repository']
-submission = submission_data[0]['submission']
-
 # Create dictionary with extracted data
 data = {
-    'repositoryUrl': repositoryUrl,
+    'repositoryUrl': repo_url,
     'actor': actor,
-    'repository': repository,
-    'submission': submission
+    'repository': repo,
+    'submission': report_data
 }
 
 # Convert dictionary to JSON string
 json_data = json.dumps(data)
 
 # Send POST request to API_URL with JSON data
-response = requests.post(API_URL, json=data)
+response = requests.post(api_url, json=data)
 
 # Print response status code and content
 print(response.status_code)
