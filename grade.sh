51ab4b0de130b999359cd2fe730df9377b92ac30
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-05-03 12:08:49 +0200

    test

--- grade.sh
+++ grade.sh
@@ -8,11 +8,7 @@ ACTOR="OTH-Digital-Skills/lab-04-mario-angie_123"
 
 echo "$API_URL" "$REPO_URL" "$REPO_URL" "$REPO_URL"
 
-curl -X POST \
-  -d '{
-      "test": "test"
-  }' \
-  http://host.docker.internal:3000/api/submission/github
+curl -X GET http://host.docker.internal:3000/api/submission/github
 
 sudo apt-get update
 apt-get install -y python3-pip
