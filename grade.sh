dcdc30cef80583573262e3ac6ebf271a390831b1
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-05-03 15:11:13 +0200

    test

--- grade.sh
+++ grade.sh
@@ -1,9 +1,6 @@
 echo "$API_URL" "$REPO_URL" "$REPO" "$ACTOR"
 
-
 curl -X GET $API_URL
-
-
 
 sudo apt-get update
 apt-get install -y python3-pip
@@ -13,6 +10,13 @@ pip3 install pytest pytest-cov pytest-json-report requests
 
 # Generate JSON report
 python -m pytest --json-report -v --json-report-indent=2
+
+CONTENTS=$(cat .report.json | jq .)
+echo "$CONTENTS"
+
+cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}' | curl -v -X POST -H "Content-Type: application/json" -d @- "$API_URL"
+
+# Call API to submit report
 
 # Remove non-printable characters from report
 # cat .report.json | tr -d '\r\n' | sed 's/[^[:print:]]//g; s|[\\/]||g' > .report_clean.json
@@ -31,14 +35,3 @@ python -m pytest --json-report -v --json-report-indent=2
 #
 #
 #curl -v -X POST "Content-Type: application/json" -d @.submission.json $API_URL
-#cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}'
-#
-#cat .submission.json | jq -s '.[0] | {repositoryUrl, actor, repository, submission}' | curl -v -X POST "Content-Type: application/json" -d @- $API_URL
-
-PAYLOAD=$(python -m pytest --json-report | jq -n --arg repoUrl "$REPO_URL" --arg actor "$ACTOR" --arg repo "$REPO" --arg contents "$(jq .)" '{repositoryUrl: $repoUrl, actor: $actor, repository: $repo, submission: $contents}')
-
-# Send POST request with payload
-echo "$PAYLOAD" | curl -v -X POST -H "Content-Type: application/json" -d @- "$API_URL"
-
-# Call API to submit report
-
